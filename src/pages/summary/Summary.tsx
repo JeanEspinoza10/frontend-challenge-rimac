f37a8e1e4614cfaf877555f9dc1ef9822e6424d2
jespinoza <jeanria10@gmail.com>
2025-10-03 09:12:03 -0500

    modificated tsconfig

--- src/pages/summary/Summary.tsx
+++ src/pages/summary/Summary.tsx
@@ -2,32 +2,43 @@ import { SummaryCard } from "@/components/cards/SummaryCard";
 import { Load } from "@/components/loaders/Load";
 import { useData } from "@/components/provider/DataProvider";
 import { Stepper } from "@/components/stepper/Stepper";
-import { useState } from "react";
-import "@/pages/summary/summary.scss"
+import { useEffect, useState } from "react";
+import "@/pages/summary/summary.scss";
 import { BackButton } from "@/components/navigation/Navigation";
+import { useNavigate } from "react-router-dom";
 
 export const Summary = () => {
   const { user } = useData();
   const [isLoad, setIsLoad] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user?.birthDay || !user?.name || !user?.lastName) {
+      navigate("/home");
+      return;
+    }
+    setIsLoad(!isLoad);
+  }, []);
+
   return (
-   <>
+    <>
       {isLoad ? (
         <Load />
       ) : (
         <main className="summary">
-          <Stepper activeLabel="Resumen"/>
-          <BackButton to="/planes"/>
+          <Stepper activeLabel="Resumen" />
+          <BackButton to="/planes" />
           <section className="summary-info">
             <SummaryCard
               userName={user.name}
               dni={user.nroDoc}
               phone={user.phone}
               planName={user.planSelect}
-              planCost={String(user.planPrice)}       
-              />
+              planCost={String(user.planPrice)}
+            />
           </section>
         </main>
       )}
     </>
-  )
-}
+  );
+};
